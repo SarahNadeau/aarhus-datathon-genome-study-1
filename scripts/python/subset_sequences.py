ae2285ef0248fba53904a8999a1859428f7492b7
Sarah Nadeau <sarah.nadeau@bsse.ethz.ch>
2021-08-21 11:24:28 +0200

    implemented config parsing, data loading

--- scripts/python/subset_sequences.py
+++ scripts/python/subset_sequences.py
@@ -1,5 +1,22 @@
 # TITLE: Subset sequences.
 # INPUT: Sequence data provided by datathon organizers.
-# OUTPUT: The sequence data, subset to samples of interest.
+# OUTPUT: List of sequence identifiers.
 
+import yaml
+import pandas as pd
+
+# Take user input: countries, lineages, date range, weekly proportion of cases to take.
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+print(config)
+
+# Read sequence metadata.
+metadata = pd.read_csv(config['path_to_data'] + "/metadata_snpeff_tidy_Denmark.tsv", sep='\t')
+print(metadata.head(1))
+
+# Import case count data
+case_count_data = pd.read_csv(config['path_to_data'] + "/owid-covid-data.csv")
+print(case_count_data.head(1))
+
+# Output list of sequence identifiers.
 
