5f1bf1fc1ccf79da7201d9255beda6d263b1c0a7
Chaoran Chen <mail@chaoran-chen.de>
2021-08-21 11:23:38 +0200

    Add code to load OWID vaccination data

---
+++ scripts/R/main.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+library(lubridate)
+source("vaccination.R")
+
+# Load data
+vacc_data <- vacc_load_data()
---
+++ scripts/R/vaccination.R
@@ -0,0 +1,46 @@
+
+vacc_load_data <- function () {
+  data <- read_csv(
+    url("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv"),
+    col_types = cols(
+      "location" = col_character(),
+      "iso_code" = col_character(),
+      "date" = col_date(),
+      "total_vaccinations" = col_double(),
+      "people_vaccinated" = col_double(),
+      "people_fully_vaccinated" = col_double(),
+      "total_boosters" = col_double(),
+      "daily_vaccinations_raw" = col_double(),
+      "daily_vaccinations" = col_double(),
+      "total_vaccinations_per_hundred" = col_double(),
+      "people_vaccinated_per_hundred" = col_double(),
+      "people_fully_vaccinated_per_hundred" = col_double(),
+      "total_boosters_per_hundred" = col_double(),
+      "daily_vaccinations_per_million" = col_double()
+    )
+  ) %>%
+    filter(location %in% c("Denmark", "Switzerland")) %>%
+    mutate(
+      date = ymd(date)
+    ) %>%
+    rename(
+      country = location
+    ) %>%
+    mutate(
+      isoyear = as.integer(isoyear(date)),
+      isoweek = as.integer(isoweek(date))
+    ) %>%
+    group_by(country, isoyear, isoweek) %>%
+    summarize(
+      people_vaccinated = max(people_vaccinated),
+      people_fully_vaccinated = max(people_fully_vaccinated),
+      total_vaccinations = max(total_vaccinations)
+    ) %>%
+    drop_na(total_vaccinations) %>%
+    replace_na(list(
+      people_vaccinated = 0,
+      people_fully_vaccinated = 0
+    ))
+
+  return(data)
+}
