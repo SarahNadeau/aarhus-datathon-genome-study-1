96b1e0d8e37a3bfc2aec33bec5a7637ef26f7f82
Sarah Nadeau <sarah.nadeau@bsse.ethz.ch>
2021-08-21 17:11:59 +0200

    Add one bash script to rule them all

---
+++ scripts/main.sh
@@ -0,0 +1,14 @@
+# TITLE: Run the pipeline
+
+mkdir -p temp
+
+# Parse the mutations file into a per-sample dictionary of nucleotide mutations
+python scripts/python/parse_mutations_file.py \
+-m data/mutations_snpeff_annotated_tidy_Switzerland.tsv \
+-o temp/parsed_mut_210821.tsv
+
+# Select genome IDs to analyze based on confirmed case data and parameters in config.yaml
+python scripts/python/subset_sequences.py
+
+# Calculate weekly diversity based on selected genome IDs
+python scripts/python/calc_diversity.py
